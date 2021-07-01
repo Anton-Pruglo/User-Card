aa8603c2acb55c591356bd4cf267bc0ba162c55d
Anton-Pruglo <xatastudio@gmail.com>
2021-07-01 22:30:18 +0300

    add Conditional Rendering

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,38 @@
 import './Card.css';
 
-export const Card = ({item}) => (
-    <div className="users">
-        <div className="card">
-            <div className="image-wrapper">
-                {/*<img className="image" src=""/>*/}
-                <h3 className="image-placeholder" style={{background: '#' + Math.floor(Math.random() * 16777215).toString(16)}}>
-                    {item.name.first[0]}
-                    {item.name.last[0]}
-                </h3>
+export const Card = ({item}) => {
+    const { picture, name, location, email } = item;
+    const { first, last } = name;
+    const { large } = picture;
+    const { country } = location;
+
+    return (
+        <div className="users">
+            <div className="card">
+                <div className="image-wrapper">
+                    {large
+                        ? <img className="image" alt="" src={large}/>
+                        : <h3 className="image-placeholder" style={{background: '#' + Math.floor(Math.random() * 16777215).toString(16)}}>
+                            {first[0]}
+                            {last[0]}
+                        </h3>
+                    }
+                </div>
+                <h1 className="name">
+                    {first}
+                    {' '}
+                    {last}
+                </h1>
+                <p className="title">
+                    {country}
+                </p>
+                <p>
+                    Email {email}
+                </p>
             </div>
-            <h1 className="name">
-                {item.name.first}
-                {' '}
-                {item.name.last}
-            </h1>
-            <p className="title">
-                {item.location.country}
-            </p>
-            <p>
-                Email {item.email}
-            </p>
         </div>
-    </div>
-)
+    )
+}
+
+
 
